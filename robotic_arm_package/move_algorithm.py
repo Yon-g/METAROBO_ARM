05aa2931fdecd580cae629458680ab933db79249
Yongki <kimyongwon987@gmail.com>
2024-09-20 19:51:06 +0900

    add gitignore

--- robotic_arm_package/move_algorithm.py
+++ robotic_arm_package/move_algorithm.py
@@ -10,30 +10,26 @@ D_DIC = {
     'ARRIVE_IDX' : 6
 }
 
-start = time.time()
+def optimal_sequence(ball_destination):
+    ball_squence = [0,1,2,3,4,5] # 공 번호 리스트
+    perms = list(itertools.permutations(ball_squence)) # 순열 생성
+    # perms: 모든 경우의 공 잡는 순서가 들어있는 리스트
 
-ball_squence = [0,1,2,3,4,5] # 공 번호 리스트
-ball_dst = ['BLUE','GREEN','RED','ERROR','ERROR','ERROR'] #공을 잡았을 때의 목적지,' 인덱스' 번호 = 공번호
-perms = list(itertools.permutations(ball_squence)) # 순열 생성
-perms_2 = list(itertools.permutations(ball_dst)) # 순열 생성
-# perms: 모든 경우의 공 잡는 순서가 들어있는 리스트
+    #각 공으로부터 도착지점까지 거리값 리스트 , 거리값 테이블 참고
+    cost_list = [
+        [26.0, 22.0, 21.0, 10.0, 15.0],
+        [23.0, 17.5, 16.5, 15.0, 18.5],
+        [20.0, 13.0, 12.0, 19.0, 22.0],
+        [24.0, 20.5, 22.0, 10.0, 19.0],
+        [20.0, 16.0, 17.0, 15.0, 21.5],
+        [16.5, 11.5, 13.5, 19.0, 25.0],
+        [34.0, 31.0, 30.0, 0.0 , 25.0]
+    ]
 
-#각 공으로부터 도착지점까지 거리값 리스트 , 거리값 테이블 참고
-cost_list = [
-    [26.0, 22.0, 21.0, 10.0, 15.0],
-    [23.0, 17.5, 16.5, 15.0, 18.5],
-    [20.0, 13.0, 12.0, 19.0, 22.0],
-    [24.0, 20.5, 22.0, 10.0, 19.0],
-    [20.0, 16.0, 17.0, 15.0, 21.5],
-    [16.5, 11.5, 13.5, 19.0, 25.0],
-    [34.0, 31.0, 30.0, 0.0 , 25.0]
-]
-
-# 공 잡는 순서와 총 비용이 들어가는 리스트
-result_list = []
-count = 0
-PATH_PRINT = False
-for ball_destination in perms_2:
+    # 공 잡는 순서와 총 비용이 들어가는 리스트
+    result_list = []
+    count = 0
+    PATH_PRINT = False
     for sequence in perms: # 공 잡는 순서의 경우의 수를 하나씩 꺼내서 총 비용 계산
         total_cost = 0 # 총 비용값 저장 변수
         n_loc = 'BUTTON' # 현재 로봇팔 위치
@@ -60,7 +56,6 @@ for ball_destination in perms_2:
 
     result_list.sort(key=lambda x:x[1], reverse=True) # 총 비용값을 기준으로 내림차순 정렬
 
-    finish = time.time()- start # 걸린 시간 측정
     worst_path = result_list[0]
     best_path = result_list[-1]
 
@@ -68,10 +63,3 @@ for ball_destination in perms_2:
     print("가장 긴 경로 비용 :", worst_path[1] )
     print("가장 좋은 공 잡는 순서 : ", best_path[0])
     print("가장 짧은 경로 비용 : ", best_path[1])
-    print("걸린시간:",finish)
-    if ball_dst[best_path[0][-1]] != "ERROR":
-        print("마지막이 에러가 아닐경우가 최선")
-        count+=1
-
-print("에러가 아닌경우 최선 개수:",count)
-print("총 경우의 수",len(perms)*len(perms_2))
